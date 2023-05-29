eb59823b9ff8cb6102de47fa9f48c50e5994a1f7
Matias Falcone <matias.falcone@gmail.com>
2023-05-29 19:41:31 +1000

    added support for mumbai

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,27 @@
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { smartWallet, localWallet, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }) {
+ 
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider 
+      activeChain={activeChain}
+      supportedWallets={[
+        smartWallet({
+          factoryAddress: "0xb2031D19907Eac16CF208Db17d2475a0A23bd920", // Address of your account factory smart contract
+          thirdwebApiKey: "fa97ae2edb3597ae5e51a3f4eab1bd69f7103ca4e8ebc751004727d10db1cb8d82894a12501a9d82fd75b269f81e181790cfe09dc99cb1733444b4e8ffce635d",
+          gasless: true,
+          // Local wallet as the only option for EOA
+          personalWallets: [
+            localWallet({
+              persist: true,
+            }),
+          ],
+        }),
+      ]}
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
